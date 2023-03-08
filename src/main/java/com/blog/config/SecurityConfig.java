21987435a3ac3a586efd86b352fab01d71882f94
aarrunyal <aarrunyal@gmail.com>
2023-03-07 20:11:01 -0500

    spring security on-going

--- src/main/java/com/blog/config/SecurityConfig.java
+++ src/main/java/com/blog/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.blog.security.CustomUserDetailService;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
 
+	
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
+	
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception  {
 
@@ -23,5 +33,15 @@ public class SecurityConfig{
 		and().
 		httpBasic();
 		return http.build();
+	}
+	
+	@Bean
+	protected void configureAuthenticationManagerBuilder(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/com/blog/entities/User.java
+++ src/main/java/com/blog/entities/User.java
@@ -1,16 +1,27 @@
 package com.blog.entities;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.aot.generate.GenerationContext;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -18,82 +29,73 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
 
-public class User {
-	
-	
 	public User() {
-		
+
 	}
-	
+
 //	validating using JSR 380 also know as Bean Validation 2.0
 //	@NotNull @Min, @Size @Max @NotNull
-	
+
 	@Id
-	@GeneratedValue(strategy  = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	@Column(name="first_name", nullable = false, length =100)
+
+	@Column(name = "first_name", nullable = false, length = 100)
 	private String firstName;
-	
-	@Column(name="last_name", nullable = false, length =100)
+
+	@Column(name = "last_name", nullable = false, length = 100)
 	private String lastName;
-	
+
 	private String password;
 	private String email;
 	private String about;
-	
+
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-	private List<Post> posts= new ArrayList<>();
-	
-	
-	public Long getId() {
-		return id;
+	private List<Post> posts = new ArrayList<>();
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "id"))
+	private Set<Role> roles = new HashSet<>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<SimpleGrantedAuthority> authorities =  roles.stream().map((role)-> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+		return authorities;
 	}
-	public void setId(Long id) {
-		this.id = id;
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.email;
 	}
-	public String getFirstName() {
-		return firstName;
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
 	}
-	public String getLastName() {
-		return lastName;
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
 	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getAbout() {
-		return about;
-	}
-	public void setAbout(String about) {
-		this.about = about;
-	}
-	
-	public List<Post> getPosts() {
-		return posts;
-	}
-	public void setPosts(List<Post> posts) {
-		this.posts = posts;
-	}
-	
-	
-	
-	
-	
+
 }
